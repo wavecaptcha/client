c03caef20a6bad28550e8c087ba7379084e2a553
WaveDev <wavedev@github.com>
2025-11-02 20:37:59 +0100

    disable logging and remove reset captcha btn

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,5 @@
 const log = (level, text) => {
+    if (true) return
     console[level]("%cWaveCaptcha%c " + text, "color: white; background-color: #007BFF; font-weight: semibold; padding: 5px 5px; border-radius: 4px;", "");
 }
 export { log }
