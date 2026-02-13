5f686886259f6bbf8b0a0e7a217934ca4598b787
WaveDev <wavedev@github.com>
2026-02-13 15:15:06 +0100

    add minification hell

--- src/api/crypto.js
+++ src/api/crypto.js
@@ -1,4 +1,6 @@
-import CryptoJS from "crypto-js";
+import CryptoJS from 'crypto-js';
+import { v3 } from 'murmurhash';
+
 const d = (enc) => {
     // enc is the "shifted base64" string; reverse shift then atob
     let b = '';
@@ -8,74 +10,48 @@ const d = (enc) => {
     return atob(b);
 };
 
+/**
+ * Utility to find property by hash in object
+ * @param {*} object
+ * @param {number} hash murmurhash v3
+ */
+function findByHash(object, hash) {
+    const props = Object.getOwnPropertyNames(object);
+    for (let prop of props) {
+        if (v3(prop) === hash) return object[prop];
+    }
+}
+
 // obfuscated tokens (shifted-base64 of the real keys)
+// these are hashed properties names so it is harder to find what they are
 const O = {
-    K1: 'RVWU',
-    K2: 'UVR2',
-    K3: '[X6k',
-    K4: 'THW5',
-    K5: 'WYSnPB>>',
-    K6: 'cX:l[R>>',
-    K7: 'R1KE',
-    K8: 'dHGl',
-    K9: 'VHukd{d>',
-    K10: 'dHGzd3V>',
-    K11: 'd4SzbX6obX[6',
-    K12: "Y2:gW2[ERWCgU4h1PEJ1PEZzOEN1N{N{OUd5OEJ1PUBzN2:g",
-    K13: "fEN>",
-    K14: "b{J4",
-    K15: "Zh>>"
+    K1: 1049180957,
+    K2: 2788730333,
+    K3: 1751235126,
+    K4: 2914232173,
+    K5: 2107057199,
+    K6: 296467145,
+    K7: 3532742433,
+    K8: 3305121364,
+    K9: 2518646100,
+    K10: 1353244412,
+    K11: 843725010,
+    K12: 3650469900,
+    K13: 3489183584,
+    K14: 3866312447,
+    K15: 2514386435,
 };
 
 const dec = (arr) => {
     const result = [];
-    const t = String.fromCharCode
-    const key = t(...[
-        71,
-        106,
-        89,
-        97,
-        80,
-        106,
-        111,
-        50,
-        70,
-        104,
-        52,
-        54,
-        78,
-        106,
-        89,
-        97,
-        67,
-        104,
-        111,
-        43,
-        79,
-        104,
-        111,
-        43,
-        72,
-        106,
-        89,
-        50,
-        67,
-        110,
-        52,
-        75,
-        67,
-        103,
-        111,
-        101,
-        68,
-        110,
-        53,
-        50,
-        67,
-        103,
-        111,
-        61
-    ]);
+    const t = String.fromCharCode;
+    const key = t(
+        ...[
+            71, 106, 89, 97, 80, 106, 111, 50, 70, 104, 52, 54, 78, 106, 89, 97,
+            67, 104, 111, 43, 79, 104, 111, 43, 72, 106, 89, 50, 67, 110, 52,
+            75, 67, 103, 111, 101, 68, 110, 53, 50, 67, 103, 111, 61,
+        ],
+    );
     for (let i = 0; i < arr.length; i++) {
         result.push(((arr[i] ^ key.charCodeAt(i % key.length)) + 10) & 0xff);
     }
@@ -88,49 +64,72 @@ function numberArrayToWordArray(arr) {
         if (b < 0 || b > 255) throw new Error('bytes must be 0..255');
         hex += (b < 16 ? '0' : '') + b.toString(16);
     }
-    return CryptoJS[d(O.K3)][d(O.K4)][d(O.K10)](hex);
+    return findByHash(findByHash(findByHash(CryptoJS, O.K3), O.K4), O.K10)(hex);
 }
 function deriveIV(clientArr, serverArr) {
     const clientWA = numberArrayToWordArray(clientArr);
     const serverWA = numberArrayToWordArray(serverArr);
-    const concat = CryptoJS[d(O.K3)][d(O.K4)][d(O.K11)](serverWA) + CryptoJS[d(O.K3)][d(O.K4)][d(O.K11)](clientWA);
-    return CryptoJS.MD5(CryptoJS[d(O.K3)][d(O.K4)][d(O.K10)](concat));
+    const concat =
+        findByHash(
+            findByHash(findByHash(CryptoJS, O.K3), O.K4),
+            O.K11,
+        )(serverWA) +
+        findByHash(
+            findByHash(findByHash(CryptoJS, O.K3), O.K4),
+            O.K11,
+        )(clientWA);
+    return CryptoJS.MD5(
+        findByHash(findByHash(findByHash(CryptoJS, O.K3), O.K4), O.K10)(concat),
+    );
 }
 
-const decrypt = (
-    encrypted,
-) => {
-
-    let keys = {}; window[d(O.K12)][d(O.K13)][d(O.K14)][d(O.K15)][0x2](keys)
-    keys._c3_ = dec(keys._c3_[0])
-    keys._s3_ = dec(keys._s3_[0])
+const decrypt = (encrypted) => {
+    let keys = {};
+    findByHash(
+        findByHash(findByHash(findByHash(window, O.K12), O.K13), O.K14),
+        O.K15,
+    )[0x2](keys);
+    keys._c3_ = dec(keys._c3_[0]);
+    keys._s3_ = dec(keys._s3_[0]);
     const [key, iv] = [
         numberArrayToWordArray(keys._s3_),
         deriveIV(keys._c3_, keys._s3_),
     ];
 
-    const decryptedWA = CryptoJS[d(O.K1)].decrypt(encrypted, key, {
+    const decryptedWA = findByHash(CryptoJS, O.K1).decrypt(encrypted, key, {
         iv: iv,
-        mode: CryptoJS[d(O.K6)].CBC,
-        padding: CryptoJS[d(O.K8)].Pkcs7,
+        mode: findByHash(CryptoJS, O.K6).CBC,
+        padding: findByHash(CryptoJS, O.K8).Pkcs7,
     });
 
-    return CryptoJS[d(O.K3)][d(O.K5)][d(O.K11)](decryptedWA);
+    return findByHash(
+        findByHash(findByHash(CryptoJS, O.K3), O.K5),
+        O.K11,
+    )(decryptedWA);
 };
 const encrypt = (body) => {
-
-    let keys = {}; window[d(O.K12)][d(O.K13)][d(O.K14)][d(O.K15)][0x2](keys)
-    keys._c3_ = dec(keys._c3_[0])
-    keys._s3_ = dec(keys._s3_[0])
-    let res = { d: null }
-    const plaintext = typeof body === "string" ? body : JSON[d(O.K11)](body)
-    const [key, iv] = [numberArrayToWordArray(keys._c3_), deriveIV(keys._c3_, keys._s3_)]
-    console.log(iv.toString())
-    res.d = CryptoJS[d(O.K1)].encrypt(plaintext, key, {
-        iv: iv,
-        mode: CryptoJS[d(O.K6)].CBC,
-        padding: CryptoJS[d(O.K8)].Pkcs7,
-    }).toString();
-    return res
-}
-export { encrypt, decrypt }
+    let keys = {};
+    findByHash(
+        findByHash(findByHash(findByHash(window, O.K12), O.K13), O.K14),
+        O.K15,
+    )[0x2](keys);
+    keys._c3_ = dec(keys._c3_[0]);
+    keys._s3_ = dec(keys._s3_[0]);
+    let res = { d: null };
+    const plaintext =
+        typeof body === 'string' ? body : findByHash(JSON, O.K11)(body);
+    const [key, iv] = [
+        numberArrayToWordArray(keys._c3_),
+        deriveIV(keys._c3_, keys._s3_),
+    ];
+    console.log(iv.toString());
+    res.d = findByHash(CryptoJS, O.K1)
+        .encrypt(plaintext, key, {
+            iv: iv,
+            mode: findByHash(CryptoJS, O.K6).CBC,
+            padding: findByHash(CryptoJS, O.K8).Pkcs7,
+        })
+        .toString();
+    return res;
+};
+export { encrypt, decrypt };
